44273fe2bdb6c6de812ece05cadca17f7283f0ce
Emmed22 <duartmary22006@gmail.com>
2026-05-29 11:37:11 -0300

    initial commit: th:each project setup

---
+++ src/main/java/com/estudos/springmvc/spring_mvc_02_th_each/SpringMvc02ThEachApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springmvc.spring_mvc_02_th_each;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc02ThEachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc02ThEachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/springmvc/spring_mvc_02_th_each/SpringMvc02ThEachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springmvc.spring_mvc_02_th_each;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc02ThEachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
